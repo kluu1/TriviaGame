2eff5c96542e733d10f16c31a3335f5176db8ca1
kle9ej <kevin_luu@homedepot.com>
2018-09-12 14:37:34 -0400

    added sound, modified questions, fixed logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
 
+    // Declare global variables
+    var correct = 0;
+    var incorrect = 0;
+    var questionNum = 0;
+    var seconds;
+    var qTimer;
+    var clickSound = new Audio('./assets/sounds/click.ogg');
+    var soundCorrect = new Audio('./assets/sounds/correct.wav');
+    var soundIncorrect = new Audio('./assets/sounds/incorrect.wav');
+
     // Get DOM elements
     var $questions = $('#questions-content');
     var $choices = $('#choices');
@@ -7,96 +17,131 @@ $( document ).ready(function() {
     var $incorrect = $('#incorrect');
     var $numCorrect = $('#numCorrect');
     var $numIncorrect = $('#numIncorrect');
-    var $done = $('#done');
     var $start = $('#start');
     var $restart = $('#restart');
     var $gameOver = $('#game-over');
+    var $timesUp = $('#timesUp');
+    var $scoreDiv = $('#score-div');
     var $correctAnswer = $('#correctAnswer');
+    var $incorrectAnswer = $('#incorrectAnswer');
+    var $displayAnswer = $('#displayAnswer');
+    var $displayTheAnswer = $('#displayTheAnswer');
     var $timer = $('#timer');  
     var $choiceA = $('#choice-A');
     var $choiceB = $('#choice-B');
     var $choiceC = $('#choice-C');
     var $choiceD = $('#choice-D');
+    var $choice = $('.choices');
 
-    // Declare variables
-    var correct = 0;
-    var incorrect = 0;
-    var questionNum = 0;
-    var seconds = 3;
-    var qTimer;
-   
     // Create an array of questions
     var questionsArr = [{
-                question: "Who was the lead singer for Led Zeppelin?",
-                choices: ["Phil Collins", "Bon Scott", "Robert Plant", "Terry Reid"],
-                correctAnswer: "Robert Plant"
+                question: "Who created JavaScript? ",
+                choices: ["Microsoft", "Sun Microsystems", "Oracle", "Netscape"],
+                correctAnswer: "Netscape"
             }, {
-                question: "Who was the guitarist for The Who?",
-                choices: ["Jimmy Page", "Pete Townshend", "Angus Young", "Jimi Hendrix"],
-                correctAnswer: "Pete Townshend"
+                question: "How long did Brendan Eich take to write the JavaScript programming language?",
+                choices: ["10 days", "2 weeks", "2 months", "6 months"],
+                correctAnswer: "10 days"
             }, {
-                question: "Who was NOT in Pink Floyd?",
-                choices: ["Roger Waters", "Syd Barrett", "Roger Daltrey", "David Gilmour"],
-                correctAnswer: "Roger Daltrey"
-            }];
+                question: "Which of following is not a reserved word in JavaScript?",
+                choices: ["default", "finally", "throw", "undefined"],
+                correctAnswer: "undefined"
+            }, {
+                question: "What is the function for creating a prompt box?",
+                choices: ["alert();", "prompt();", "document.prompt();", "document.ask();"],
+                correctAnswer: "prompt();"
+            }, {
+                question: "Which of the following events is not a valid event?",
+                choices: ["onLoad", "onClick", "onLinkClick", "onMouseover"],
+                correctAnswer: "onLinkClick"
+            }, {
+                question: "Which of the following function adds and/or removes elements from an array?",
+                choices: ["toSource()", "sort()", "splice()", "unshift()"],
+                correctAnswer: "splice()"
+            }, {
+                question: "Which of the following function returns the number's value?",
+                choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+                correctAnswer: "valueOf()"
+            }, {
+                question: "Which built-in method calls a function for each element in the array?",
+                choices: ["while()", "loop()", "forEach()", "None of the above"],
+                correctAnswer: "forEach()"
+            }, {
+                question: "How do you create a function in JavaScript?",
+                choices: ["function myFunction()", "loop()", "function:myFunction()", "func myFunction()"],
+                correctAnswer: "function myFunction()"
+            }, {
+                question: "How does a FOR loop start?",
+                choices: ["for i = 1 to 5", "for (i = 0; i <= 5; i++)", "for (i <= 5; i++)", "for (i = 0; i <= 5)"],
+                correctAnswer: "for (i = 0; i <= 5; i++)"
+            }
+        ];
 
     // Hide elements at startup
     $correct.hide();
     $incorrect.hide();
     $choices.hide();
-    $done.hide();
+    $timesUp.hide();
+    $scoreDiv.hide();
+    $correctAnswer.hide();
+    $incorrectAnswer.hide();
+    $displayAnswer.hide();
     $restart.hide();
-    $gameOver.hide();  
+    $gameOver.hide(); 
 
-    // Start the game and start event listners
+    // Start the game by starting event listners
     function start() {
         $start.on('click', function() {
+            clickSound.play();
             $start.hide();
             $restart.hide();
             $gameOver.hide();
+            $scoreDiv.show();
             $choices.show();
             $correct.show();
             $incorrect.show();
-            $done.show();
             displayQuestions();
         });
-        $done.on('click', function() {
-            clearInterval(qTimer);
-            game.check();
-        });
-        $restart.on('click', function(){
+        $restart.on('click', function() {
+            clickSound.play();
             restartGame();
-        }); 
+        });
+        $choice.on('click', choiceEvent); 
     }
 
-    // Display the question
+    function choiceEvent() {
+        clearInterval(qTimer);
+        choice = $(this); 
+        checkSelected();
+    }
+
+    // Display the question along with choices
     function displayQuestions() {
-
+        $scoreDiv.show();
         $questions.html(questionsArr[questionNum].question);
-
         $choiceA.append(questionsArr[questionNum].choices[0]);
         $choiceB.append(questionsArr[questionNum].choices[1]);
         $choiceC.append(questionsArr[questionNum].choices[2]);
         $choiceD.append(questionsArr[questionNum].choices[3]);
 
-        // // Loops through the array of choices and display it
-        // for (i = 0; i < questionsArr[questionNum].choices.length; i++) {
-        //     $choices.append('<h3><input type="radio" name="question' + '-' + questionNum + '"value="' + questionsArr[questionNum].choices[i] + '">' + questionsArr[questionNum].choices[i] + '</h3>');  
-        // }
-
-        // Set and start timer
-        seconds = 3;
+        // Start the timer for current question
+        seconds = 1;
         $timer.html(seconds);
         qTimer = setInterval(countDown, 1000);
-
     }
 
+    // Decrement seconds
     function countDown() {
         seconds--;
         $timer.html(seconds);
 
+        // Logic for when time runs out
         if (seconds <= 0) {
-            $correctAnswer.append('<h3>Oops.. Times up! <br> The answer is "' + questionsArr[questionNum].correctAnswer + '"</h3>');
+            $choice.off('click', choiceEvent); 
+            soundIncorrect.play();
+            $timesUp.show();
+            $displayAnswer.show();
+            $displayTheAnswer.html(questionsArr[questionNum].correctAnswer);
             clearInterval(qTimer);
             incorrect++;
             $numIncorrect.html(incorrect);
@@ -104,6 +149,7 @@ $( document ).ready(function() {
         }
     }
 
+    // Prepare for next question
     function launchNextQuestion() {
         setTimeout(function() {
             questionNum++;
@@ -112,31 +158,67 @@ $( document ).ready(function() {
         }, 3000);
     }
 
-    function nextQuestion() {
-        if (questionNum === questionsArr.length) {
-            endGame();
-        } else {
-            displayQuestions();
-        }
-    }
-
     function clear() {
         $questions.empty();
         $choiceA.empty();
         $choiceB.empty();
         $choiceC.empty();
         $choiceD.empty();
-        $correctAnswer.empty();
+        $timesUp.hide();
+        $correctAnswer.hide();
+        $incorrectAnswer.hide();
+        $displayAnswer.hide();
     }
 
+    // Logic to see if there are more questions or end game
+    function nextQuestion() {
+        if (questionNum === questionsArr.length) {
+            endGame();
+        } else {
+            displayQuestions();
+            $choice.on('click', choiceEvent); 
+        }
+    }
+
+    // Check if selected answer is correct
+    function checkSelected() {
+        $choice.off('click', choiceEvent); 
+        if (choice.text() === questionsArr[questionNum].correctAnswer) {
+            soundCorrect.play();
+            isCorrect();
+        } else {
+            soundIncorrect.play();
+            isIncorrect();
+        }
+    }
+
+    // If selected answer is correct
+    function isCorrect() {
+        correct++;
+        $numCorrect.html(correct);
+        $correctAnswer.show();
+        launchNextQuestion();
+    }
+
+    // If selected anwer is incorrect
+    function isIncorrect() {
+        incorrect++;
+        $numIncorrect.html(incorrect);
+        $incorrectAnswer.show();
+        $displayAnswer.show();
+        $displayTheAnswer.html(questionsArr[questionNum].correctAnswer);
+        launchNextQuestion();
+    }
+
+    // Function to end the game
     function endGame() {
         $timer.hide();
-        $done.hide();
         $choices.hide();
         $gameOver.show();
         $restart.show();
     }
 
+    // Function to restart the game
     function restartGame() {
         correct = 0;
         incorrect = 0;
@@ -146,14 +228,16 @@ $( document ).ready(function() {
         $start.hide();
         $restart.hide();
         $gameOver.hide();
+        $scoreDiv.show();
         $choices.show();
         $correct.show();
         $timer.show();
         $incorrect.show();
-        $done.show();
+        $choice.on('click', choiceEvent); 
         displayQuestions();
     }
 
+    // Starts the game
     start();
 
 });
