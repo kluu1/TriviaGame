61c6a2b5562a0b6aaf7a08c5009cd44a5a8312dd
kle9ej <kevin_luu@homedepot.com>
2018-09-13 10:43:29 -0400

    fixed quotes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,47 +35,49 @@ $( document ).ready(function() {
 
     // Create an array of questions
     var questionsArr = [{
-                question: "Who created JavaScript? ",
-                choices: ["Microsoft", "Sun Microsystems", "Oracle", "Netscape"],
-                correctAnswer: "Netscape"
+                question: 'Who created JavaScript?',
+                choices: ['Microsoft', 'Sun Microsystems', 'Oracle', 'Netscape'],
+                correctAnswer: 'Netscape'
             }, {
-                question: "How long did Brendan Eich take to write the JavaScript programming language?",
-                choices: ["10 days", "2 weeks", "2 months", "6 months"],
-                correctAnswer: "10 days"
+                question: 'How long did Brendan Eich take to write the JavaScript programming language?',
+                choices: ['10 days', '2 weeks', '2 months', '6 months'],
+                correctAnswer: '10 days'
             }, {
-                question: "Which of following is not a reserved word in JavaScript?",
-                choices: ["default", "finally", "throw", "undefined"],
-                correctAnswer: "undefined"
+                question: 'Which of following is not a reserved word in JavaScript?',
+                choices: ['default', 'finally', 'throw', 'undefined'],
+                correctAnswer: 'undefined'
             }, {
-                question: "What is the function for creating a prompt box?",
-                choices: ["alert();", "prompt();", "document.prompt();", "document.ask();"],
-                correctAnswer: "prompt();"
+                question: 'What is the function for creating a prompt box?',
+                choices: ['alert();', 'prompt();', 'document.prompt();', 'document.ask();'],
+                correctAnswer: 'prompt();'
             }, {
-                question: "Which of the following events is not a valid event?",
-                choices: ["onLoad", "onClick", "onLinkClick", "onMouseover"],
-                correctAnswer: "onLinkClick"
+                question: 'Which of the following events is not a valid event?',
+                choices: ['onLoad', 'onClick', 'onLinkClick', 'onMouseover'],
+                correctAnswer: 'onLinkClick'
             }, {
-                question: "Which of the following function adds and/or removes elements from an array?",
-                choices: ["toSource()", "sort()", "splice()", "unshift()"],
-                correctAnswer: "splice()"
+                question: 'Which of the following function adds and/or removes elements from an array?',
+                choices: ['toSource()', 'sort()', 'splice()', 'unshift()'],
+                correctAnswer: 'splice()'
             }, {
-                question: "Which of the following function returns the number's value?",
-                choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
-                correctAnswer: "valueOf()"
+                question: 'Which of the following function returns the number\'s value?',
+                choices: ['toString()', 'valueOf()', 'toLocaleString()', 'toPrecision()'],
+                correctAnswer: 'valueOf()'
             }, {
-                question: "Which built-in method calls a function for each element in the array?",
-                choices: ["while()", "loop()", "forEach()", "None of the above"],
-                correctAnswer: "forEach()"
+                question: 'Which built-in method calls a function for each element in the array?',
+                choices: ['while()', 'loop()', 'forEach()', 'None of the above'],
+                correctAnswer: 'forEach()'
             }, {
-                question: "How do you create a function in JavaScript?",
-                choices: ["function myFunction()", "loop()", "function:myFunction()", "func myFunction()"],
-                correctAnswer: "function myFunction()"
+                question: 'How do you create a function in JavaScript?',
+                choices: ['function myFunction()', 'loop()', 'function:myFunction()', 'func myFunction()'],
+                correctAnswer: 'function myFunction()'
             }, {
-                question: "How does a FOR loop start?",
-                choices: ["for i = 1 to 5", "for (i = 0; i <= 5; i++)", "for (i <= 5; i++)", "for (i = 0; i <= 5)"],
-                correctAnswer: "for (i = 0; i <= 5; i++)"
+                question: 'How does a FOR loop start?',
+                choices: ['for i = 1 to 5', 'for (i = 0; i <= 5; i++)', 'for (i <= 5; i++)', 'for (i = 0; i <= 5)'],
+                correctAnswer: 'for (i = 0; i <= 5; i++)'
             }
         ];
+
+    var displayCorrectArr = []
 
     // Hides elements at startup
     function startUp (){
@@ -112,6 +114,7 @@ $( document ).ready(function() {
         $choice.on('click', choiceEvent); 
     }
 
+    // Logic for selected answer choice
     function choiceEvent() {
         clearInterval(qTimer);
         choice = $(this); 
@@ -161,6 +164,7 @@ $( document ).ready(function() {
         }, 3000);
     }
 
+    // Function to reset the DOM for next question
     function clear() {
         $questions.empty();
         $choiceA.empty();
